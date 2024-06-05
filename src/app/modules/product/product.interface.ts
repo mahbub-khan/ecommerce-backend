22a6569a6d38fc5d8a3bcbe30f8281257cf8f4b4
mahbub-khan <mrkict39@gmail.com>
2024-06-05 20:27:15 +0200

    Added product interface and schema

---
+++ src/app/modules/product/product.interface.ts
@@ -0,0 +1,19 @@
+export type TVariants = {
+  type: 'size' | 'color' | 'style';
+  value: string;
+};
+
+export type TInventory = {
+  quantity: number;
+  inStock: boolean;
+};
+
+export type TProduct = {
+  name: string;
+  description: string;
+  price: number;
+  category: string;
+  tags: string[];
+  variants: TVariants[];
+  inventory: TInventory;
+};
