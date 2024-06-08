ea71b887da5de26d0b853e48d17e3299e21fa233
mahbub-khan <mrkict39@gmail.com>
2024-06-08 17:26:46 +0200

    response added for undefined routes

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express, { Application, Request, Response } from 'express';
+import express, { Application, NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 import { ProductRoutes } from './app/modules/product/product.route';
 import { OrderRoutes } from './app/modules/order/order.route';
@@ -13,6 +13,15 @@ app.use('/api/products', ProductRoutes);
 
 //Order related routes
 app.use('/api/orders', OrderRoutes);
+
+//undefined routes
+app.use((req: Request, res: Response, next:NextFunction)=>{
+  const error = new Error("Route not found")
+  res.status(404).json({
+    success: false,
+    message: error.message
+  })
+})
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hi, Welcome to my App');
